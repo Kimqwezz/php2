818fae45898c1c987739df03cc4cf2ae16984a38
kimqwezz <kim569653@mail.ru>
2023-01-27 13:49:37 +0600

    1.4 Select and insert in one html

--- main.php
+++ main.php
@@ -8,35 +8,40 @@
 </head>
 <body>
 <?php
-// if (isset($_POST["username"]) && isset($_POST["userage"])) {
+if (isset($_POST["username"]) && isset($_POST["userage"])) {
 
-//     $username = $_POST["username"];
-//     $userage = $_POST["userage"];
-    try {
-        $conn = new PDO("mysql:host=127.0.0.1; port=3305; dbname=phphp", "root", "root");
-        $sql = "SELECT * FROM Users";
-        $result = $conn->query($sql);
-        echo "<table><tr><th>ID</th><th>Name</th><th>Age</th></tr>";
-        while($row = $result->fetch()){
-            echo "<tr>";
-                echo "<td>" . $row["id"] . "</td>";
-                echo "<td>" . $row["name"] . "</td>";
-                echo "<td>" . $row["age"] . "</td>";
-            echo "</tr>";
-        }
+    $username = $_POST["username"];
+    $userage = $_POST["userage"];
+    $conn = new PDO("mysql:host=127.0.0.1; port=3305; dbname=phphp", "root", "root");
+    
+    $sqlInsert = "INSERT INTO Users (name, age) VALUES ('$username', $userage)";
+    $resultInsert = $conn->exec($sqlInsert); 
+};
+
+function outputDataFromDB()
+{
+    $conn = new PDO("mysql:host=127.0.0.1; port=3305; dbname=phphp", "root", "root");
+    $sqlSelect = "SELECT * FROM Users";
+    $resultSelect = $conn->query($sqlSelect); 
+    echo "<table><tr><th>ID</th><th>Name</th><th>Age</th></tr>";
+    while($row = $resultSelect->fetch()){
+        echo "<tr>";
+            echo "<td>" . $row["id"] . "</td>";
+            echo "<td>" . $row["name"] . "</td>";
+            echo "<td>" . $row["age"] . "</td>";
+        echo "</tr>";
     }
-    catch (PDOException $e) {
-        echo "Database error: " . $e->getMessage();
-    }
-//}
+}
+
+outputDataFromDB();
 ?>
-<!-- <h3>Create a new User</h3>
+<h3>Create a new User</h3>
 <form method="post">
     <p>User Name:
     <input type="text" name="username" /></p>
     <p>User Age:
     <input type="number" name="userage" /></p>
     <input type="submit" value="Save">
-</form> -->
+</form>
 </body>
 </html>
