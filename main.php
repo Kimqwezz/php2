b36a098218eb2c643ed744d1ff4cb2c2242ed64f
kimqwezz <kim569653@mail.ru>
2023-01-27 12:03:26 +0600

    1.3 select

--- main.php
+++ main.php
@@ -1,15 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHPHPHPHP</title>
+</head>
+<body>
 <?php
-$business = [
-    'name' => 'K-cosmetics',
-    'goods' => [
-        'Shampoo' => 220,
-        'Foam' => 919,
-        'Sunproof cream' => 175,
-        'Essence' => 2_200
-    ]
-]; 
+// if (isset($_POST["username"]) && isset($_POST["userage"])) {
 
-
-
-require "html.php";
+//     $username = $_POST["username"];
+//     $userage = $_POST["userage"];
+    try {
+        $conn = new PDO("mysql:host=127.0.0.1; port=3305; dbname=phphp", "root", "root");
+        $sql = "SELECT * FROM Users";
+        $result = $conn->query($sql);
+        echo "<table><tr><th>ID</th><th>Name</th><th>Age</th></tr>";
+        while($row = $result->fetch()){
+            echo "<tr>";
+                echo "<td>" . $row["id"] . "</td>";
+                echo "<td>" . $row["name"] . "</td>";
+                echo "<td>" . $row["age"] . "</td>";
+            echo "</tr>";
+        }
+    }
+    catch (PDOException $e) {
+        echo "Database error: " . $e->getMessage();
+    }
+//}
 ?>
+<!-- <h3>Create a new User</h3>
+<form method="post">
+    <p>User Name:
+    <input type="text" name="username" /></p>
+    <p>User Age:
+    <input type="number" name="userage" /></p>
+    <input type="submit" value="Save">
+</form> -->
+</body>
+</html>
